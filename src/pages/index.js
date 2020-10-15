e3a4eeb016fb24e1b3dc5456dad60b3abb564b2d
Trevor Blades <tdblades@gmail.com>
2020-10-14 22:55:04 -0700

    Rework text positioning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React, {useState} from 'react';
 import getShareImage from '@jlengstorf/get-share-image';
 import {
@@ -13,6 +12,10 @@ import {
   NumberInputField,
   NumberInputStepper,
   Select,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
   Stack
 } from '@chakra-ui/core';
 import {Helmet} from 'react-helmet';
@@ -59,24 +62,17 @@ const [imageConfig, titleConfig, taglineConfig] = getShareImage({
 
 const defaultTextSpacing = titleConfig.y - (imageConfig.h - taglineConfig.y);
 
-function TextArea({textAreaWidth, textLeftOffset, ...props}) {
+function TextArea(props) {
   return (
     <Box
       borderColor="gray.500"
       borderWidth="1px"
       borderStyle="dotted"
       position="absolute"
-      w={textAreaWidth}
-      left={textLeftOffset}
       {...props}
     />
   );
 }
-
-TextArea.propTypes = {
-  textAreaWidth: PropTypes.number.isRequired,
-  textLeftOffset: PropTypes.number.isRequired
-};
 
 export default function App() {
   const data = useStaticQuery(
@@ -94,16 +90,21 @@ export default function App() {
 
   const [title, setTitle] = useState('This is a title');
   const [tagline, setTagline] = useState('Lorem ipsum dolor set amit');
-  const [textYPosition, setTextYPosition] = useState(
-    titleConfig.y + defaultTextSpacing / 2
+  const [textPosition, setTextPosition] = useState(
+    (taglineConfig.y - defaultTextSpacing / 2) / imageConfig.h
   );
   const [textSpacing, setTextSpacing] = useState(defaultTextSpacing);
   const [textLeftOffset, setTextLeftOffset] = useState(titleConfig.x);
-  const [textAreaWidth, setTextAreaWidth] = useState(titleConfig.w);
+  const [textRightOffset, setTextRightOffset] = useState(
+    imageConfig.w - titleConfig.w - titleConfig.x
+  );
   const [imageWidth, setImageWidth] = useState(imageConfig.w);
   const [imageHeight, setImageHeight] = useState(imageConfig.h);
 
-  const textAreaProps = {textAreaWidth, textLeftOffset};
+  const textAreaProps = {
+    w: imageWidth - textLeftOffset - textRightOffset,
+    left: textLeftOffset
+  };
 
   return (
     <>
@@ -159,19 +160,6 @@ export default function App() {
             </NumberInput>
           </FormControl>
           <FormControl>
-            <FormLabel>Text area width</FormLabel>
-            <NumberInput
-              value={textAreaWidth}
-              onChange={(string, number) => setTextAreaWidth(number)}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-          <FormControl>
             <FormLabel>Text left offset</FormLabel>
             <NumberInput
               value={textLeftOffset}
@@ -185,10 +173,10 @@ export default function App() {
             </NumberInput>
           </FormControl>
           <FormControl>
-            <FormLabel>Text Y position</FormLabel>
+            <FormLabel>Text right offset</FormLabel>
             <NumberInput
-              value={textYPosition}
-              onChange={(string, number) => setTextYPosition(number)}
+              value={textRightOffset}
+              onChange={(string, number) => setTextRightOffset(number)}
             >
               <NumberInputField />
               <NumberInputStepper>
@@ -196,6 +184,23 @@ export default function App() {
                 <NumberDecrementStepper />
               </NumberInputStepper>
             </NumberInput>
+          </FormControl>
+          <FormControl>
+            <FormLabel>
+              Text Y position: {Math.round(textPosition * 100)} %
+            </FormLabel>
+            <Slider
+              min={0}
+              max={1}
+              step={0.01}
+              value={textPosition}
+              onChange={setTextPosition}
+            >
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
           </FormControl>
           <FormControl>
             <FormLabel>Text spacing</FormLabel>
@@ -231,16 +236,17 @@ export default function App() {
           >
             <TextArea
               {...textAreaProps}
-              bottom={textYPosition + textSpacing / 2}
+              top={imageHeight * textPosition - textSpacing / 2}
               fontFamily={titleConfig.font}
               fontSize={titleConfig.size}
               color={'#' + titleConfig.color}
+              transform="translateY(-100%)"
             >
               {title}
             </TextArea>
             <TextArea
               {...textAreaProps}
-              top={imageHeight - textYPosition + textSpacing / 2}
+              top={imageHeight * textPosition + textSpacing / 2}
               fontFamily={taglineConfig.font}
               fontSize={taglineConfig.size}
               color={'#' + taglineConfig.color}
